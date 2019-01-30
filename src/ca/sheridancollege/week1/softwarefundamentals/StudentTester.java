755e17f154bbf22e55b9c3237c01524f3ccd7c3a
Anousheh Niazi <NIAZIAN @ sheridancollege.ca>
2019-01-30 11:36:34 -0500

    3 commit

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentTester.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentTester.java
@@ -13,13 +13,14 @@ public class StudentTester {
     
     public static void main(String[] args) {
         
-        Student [] stuList = new Student[5];
+        Student [] stuList = new Student[6];
         
         stuList[0]= new Student("Stu1",121);
         stuList[1]= new Student("Stu2",122);
         stuList[2]= new Student("Stu3",123);
         stuList[3]= new Student("Stu4",124);
         stuList[4]= new Student("Stu5",125);
+        stuList[5]= new Student("Stu5",125);
         
         
         for (int i=0;i<stuList.length;i++)
